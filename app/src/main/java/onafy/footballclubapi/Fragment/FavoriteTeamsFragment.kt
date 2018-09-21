f841139a4351cb4a3cdacf191f6f8eac1aa1a785
azizahnovitami <ccghy@gmail.com>
2018-09-21 19:29:02 +0800

    implement sqlite and favorite

--- app/src/main/java/onafy/footballclubapi/Fragment/FavoriteTeamsFragment.kt
+++ app/src/main/java/onafy/footballclubapi/Fragment/FavoriteTeamsFragment.kt
@@ -1,19 +1,86 @@
 package onafy.footballclubapi.Fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import onafy.footballclubapi.DetailTeam.TeamsDetailActivity
+import onafy.footballclubapi.Favorite
+import onafy.footballclubapi.Favorites.FavoriteTeamsAdapter
 
 import onafy.footballclubapi.R
+import onafy.footballclubapi.R.color.colorAccent
+import onafy.footballclubapi.database
+import org.jetbrains.anko.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.select
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.support.v4.onRefresh
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
+
 class FavoriteTeamsFragment : Fragment() {
+    private var favorites: MutableList<Favorite> = mutableListOf()
+    private lateinit var adapter: FavoriteTeamsAdapter
+    private lateinit var listEvent: RecyclerView
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        adapter = FavoriteTeamsAdapter(favorites){
+            ctx.startActivity<TeamsDetailActivity>("id" to "${it.teamId}")
+        }
+        listEvent.adapter = adapter
+        showFavorite()
+        swipeRefresh.onRefresh {
+            favorites.clear()
+            showFavorite()
+        }
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_teams, container, false)
+        return createView(AnkoContext.create(ctx))
+    }
+
+
+   fun createView(ui: AnkoContext<Context>): View = with(ui){
+        linearLayout {
+            lparams (width = matchParent, height = wrapContent)
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(colorAccent,
+                        android.R.color.holo_green_light,
+                        android.R.color.holo_orange_light,
+                        android.R.color.holo_red_light)
+
+                listEvent = recyclerView {
+                    lparams (width = matchParent, height = wrapContent)
+                    layoutManager = LinearLayoutManager(ctx)
+                }
+            }
+        }
+    }
+
+    private fun showFavorite(){
+        context?.database?.use {
+            swipeRefresh.isRefreshing = false
+            val result = select(Favorite.TABLE_FAVORITE)
+            val favorite = result.parseList(classParser<Favorite>())
+            favorites.addAll(favorite)
+            adapter.notifyDataSetChanged()
+        }
     }
 
 
